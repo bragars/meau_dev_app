12c2c4f2e08853e40ec13f98c9fa6e93fcf9a930
Silva <octavio.silva@vetta.digital>
2023-05-09 19:56:25 -0300

    Create home screen

--- meau_dev-app/src/routes/routes.js
+++ meau_dev-app/src/routes/routes.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Login from "../views/register/login";
 import AnimalRegisterScreen from "../views/register/animalRegister";
 import PersonalRegisterScreen from "../views/register/personalRegister";
-import NotAuthErrorScreen from "../views/notAuthErrorScreen";
+import HomeScreen from "../views/home/home";
 
 
 const Drawer = createDrawerNavigator();
@@ -13,7 +13,7 @@ const Stack = createNativeStackNavigator();
 function DrawerNavigation() {
     return (
         <Drawer.Navigator>
-            <Drawer.Screen name='Home' component={NotAuthErrorScreen} />
+            <Drawer.Screen name='Home' component={HomeScreen} />
             <Drawer.Screen name='Login' component={Login} />
             <Drawer.Screen name="Cadastro Pessoal" component={PersonalRegisterScreen} />
             <Drawer.Screen name="Cadastro Animal" component={AnimalRegisterScreen} />
